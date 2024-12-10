618869ea0f8990470aaea5aca015000702259bfc
Lacroix Marceau <164542445+Eaucram@users.noreply.github.com>
2024-12-10 16:48:02 +0100

    patch Human

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,26 +1,34 @@
-/
- 
-Décrivez votre classe HumanPlayer ici.*
-@author (votre nom)
-@version (un numéro de version ou une date)*/
+
+/**
+ * Décrivez votre classe HumanPlayer ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
 public class HumanPlayer extends Player
 {
     // variables d'instance - remplacez l'exemple qui suit par le vôtre
     private int x;
 
-    /
-     
-Constructeur d'objets de classe HumanPlayer*/
-  public HumanPlayer(int id,String name){// initialisation des variables d'instance
-      super(id,name);}
+    /**
+     * Constructeur d'objets de classe HumanPlayer
+     */
+    public HumanPlayer(int id,String name)
+    {
+        // initialisation des variables d'instance
+        super(id,name);
+    }
 
     /**
-     
-Un exemple de méthode - remplacez ce commentaire par le vôtre*
-@param  y   le paramètre de la méthode
-@return     la somme de x et de y*/
-public void chooseAction(){
-}
-public void chooseDiscardingTokens(){
-}
+     * Un exemple de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y   le paramètre de la méthode
+     * @return     la somme de x et de y
+     */
+    public void chooseAction(){
+        
+    }
+    public void chooseDiscardingTokens(){
+        
+    }
 }
