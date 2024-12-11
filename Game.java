21379772a21a55125e6e1c511aae4c72cd8bc441
Erinnom <maxime@archlinux.org>
2024-12-11 18:07:04 +0100

    test

--- Game.java
+++ Game.java
@@ -58,7 +58,7 @@ public class Game {
                 }
             }
         } else {
-            throw new IllegalArgumentException("nombre de joueurs incorrect");
+            throw new IllegalArgumentException();
         }
     }
 
